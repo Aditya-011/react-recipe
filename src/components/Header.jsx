72ca31123f1e82b6e3e5c0c746f98a0d229f64a3
Aditya-011 <adihgr18@gmail.com>
2021-04-23 15:32:12 +0530

    api added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
-
+import axios from "axios";
 function Header(props) {
+  const [state, setState] = React.useState([]);
   const [Query, setQuery] = React.useState("");
+  const getdata = async () => {
+    fetch(
+      //"https://api.spoonacular.com/recipes/random?apiKey=ad0bf0be4ce04adbbcf887e87b2973bd",
+      "https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=ad0bf0be4ce04adbbcf887e87b2973bd"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
     <div className="header">
       <h1>Recipie App</h1>
@@ -14,7 +24,12 @@ function Header(props) {
           console.log(Query);
         }}
       />
-      <button>Search</button>
+      <button onClick={getdata}>Search</button>
+      <ul>
+        {state.map((person) => (
+          <li>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
