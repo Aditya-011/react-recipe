35a28c3f63e85d8b15250ebadb3a47852e3ddb30
Aditya-011 <adihgr18@gmail.com>
2021-04-26 12:32:12 +0530

    react-router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
+import IndRecipie from "./components/IndRecipie";
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/" component={Header} exact></Route>
+          <Route path="/individual" component={IndRecipie} exact></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
