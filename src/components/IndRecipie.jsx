6634afb3e51a1e64ef28e962242dbda038affaba
Aditya-011 <adihgr18@gmail.com>
2021-04-26 13:05:39 +0530

    individual  recipie component populated

--- src/components/IndRecipie.jsx
+++ src/components/IndRecipie.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
+import MainCard from "./MainCard";
+import { Link } from "react-router-dom";
 
 function IndRecipie() {
-  return <div></div>;
+  const [data, setdata] = React.useState({});
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        "https://api.spoonacular.com/recipes/654959/information?apiKey=ad0bf0be4ce04adbbcf887e87b2973bd"
+      );
+      //console.log(res.data.recipes[0]);
+      console.log(res.data);
+      setdata(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const renderData = (obj) => {
+    //console.log(Object.keys(data).length);
+    if (Object.keys(obj).length) {
+      return <MainCard data={obj}></MainCard>;
+    }
+  };
+  React.useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <div>
+      <button>
+        <Link>Go back</Link>
+      </button>
+      {renderData(data)}
+    </div>
+  );
 }
 
 export default IndRecipie;
--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function SearchResult(props) {
   const data = props.data;
   console.log(data);
   return (
     <div>
       <li>
-        <a href="/individual">
+        <Link to="/individual">
           <img src={data.image} alt="" />
           <p>Name : {data.title}</p>
-        </a>
+        </Link>
       </li>
     </div>
   );
