e835a0220473154444e401cc5034d5894586bcc8
Aditya-011 <adihgr18@gmail.com>
2021-04-28 12:54:14 +0530

    empty query error handled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
 function Navbar() {
   const [Query, setQuery] = React.useState("");
 
   return (
     <div>
+      <Toaster position="top-right" reverseOrder={false} />
       <h1>Recipie App</h1>
       <input
         type="text"
@@ -15,8 +17,14 @@ function Navbar() {
           console.log(Query);
         }}
       />
-      <button>
-        <Link to={`search/${Query}`}>Search</Link>
+      <button
+        onClick={() => {
+          if (!Query.length) {
+            toast.error("Please Enter a Query 🙂");
+          }
+        }}
+      >
+        <Link to={Query.length ? `search/${Query}` : ""}>Search</Link>
       </button>
     </div>
   );
